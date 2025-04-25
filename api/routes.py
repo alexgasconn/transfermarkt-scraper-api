f7b16ec430925d3f7c6f02ab09e7dcd2eefef88f
Alex <alex.gascon1999@gmail.com>
2025-04-25 12:53:34 +0200

    docker first commit

--- api/routes.py
+++ api/routes.py
@@ -1,7 +1,8 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Query
 from sqlalchemy.orm import Session
 from db.database import SessionLocal
 from db.models import Player
+from typing import Optional, List
 
 router = APIRouter()
 
@@ -13,5 +14,32 @@ def get_db():
         db.close()
 
 @router.get("/players")
-def get_players(db: Session = Depends(get_db)):
-    return db.query(Player).all()
+def get_players(
+    name: Optional[str] = Query(None),
+    position: Optional[str] = Query(None),
+    nationality: Optional[str] = Query(None),
+    club: Optional[str] = Query(None),
+    db: Session = Depends(get_db)
+):
+    query = db.query(Player)
+    if name:
+        query = query.filter(Player.name.ilike(f"%{name}%"))
+    if position:
+        query = query.filter(Player.position.ilike(f"%{position}%"))
+    if nationality:
+        query = query.filter(Player.nationality.ilike(f"%{nationality}%"))
+    if club:
+        query = query.filter(Player.club.ilike(f"%{club}%"))
+    return query.all()
+
+
+@router.get("/top10")
+def get_top_10_players(db: Session = Depends(get_db)):
+    players = (
+        db.query(Player)
+        .filter(Player.market_value_eur > 0)
+        .order_by(Player.market_value_eur.desc())
+        .limit(10)
+        .all()
+    )
+    return players
